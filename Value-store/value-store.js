f5bca6790315ee5630253bcb88c468be050f1e13
Ashutosh Kumawat <aurecordsofficial@gmail.com>
2023-06-04 09:40:34 +0530

    value-store page done

---
+++ Value-store/value-store.js
@@ -0,0 +1,310 @@
+var productArray3 = [{
+    id : 1,
+    name : "Everherb Karela Jamun Juice",
+    img_url : "https://cdn01.pharmeasy.in/dam/products_otc/L79986/everherb-karela-jamun-juice-helps-maintains-healthy-sugar-levels-helps-in-weight-management-1l-2-1680172214.jpg?dim=700x0&dpr=1&q=100",
+    price : 399,
+    finalPrice : 207.50,
+    btn : "Add To Cart",
+    img1 :"https://cdn01.pharmeasy.in/dam/products_otc/L79986/everherb-karela-jamun-juice-helps-maintains-healthy-sugar-levels-helps-in-weight-management-1l-2-1680172214.jpg?dim=100x0&dpr=1&q=100",
+    img2 :"https://cdn01.pharmeasy.in/dam/products_otc/L79986/everherb-karela-jamun-juice-helps-maintains-healthy-sugar-levels-helps-in-weight-management-1l-6.1-1680172167.jpg?dim=100x0&dpr=1&q=100",
+    quantity : 1,
+    total : '207.50'  
+},
+{   id : 2,
+    name : "Everherb Diabetic Care Juice",
+    img_url : "https://cdn01.pharmeasy.in/dam/products_otc/V68813/everherb-diabetic-care-juice-11-potent-ingredients-healthy-blood-sugar-management-1-litre-bottle-2-1680238244.jpg?dim=700x0&dpr=1&q=100",
+    price : 599,
+    finalPrice : 299,
+    btn : "Add To Cart",
+    img1 :"https://cdn01.pharmeasy.in/dam/products_otc/V68813/everherb-diabetic-care-juice-11-potent-ingredients-healthy-blood-sugar-management-1-litre-bottle-2-1680238244.jpg?dim=100x0&dpr=1&q=100",
+    img2 :"https://cdn01.pharmeasy.in/dam/products_otc/V68813/everherb-diabetic-care-juice-11-potent-ingredients-healthy-blood-sugar-management-1-litre-bottle-6.1-1680172286.jpg?dim=100x0&dpr=1&q=100",
+    quantity : 1,
+    total : '299'
+   
+},
+
+{   id : 3,
+    name : "Everherb Aloe Vera Juice",
+    img_url : "https://cdn01.pharmeasy.in/dam/products_otc/E54755/everherb-by-pharmeasy-aloe-vera-juice-with-pulp-rejuvenates-skin-hair-1-litre-bottle-2-1680172150.jpg?dim=700x0&dpr=1&q=100",
+    price : 399.50,
+    finalPrice : 199.00,
+    btn : "Add To Cart",
+    img1 :"https://cdn01.pharmeasy.in/dam/products_otc/E54755/everherb-by-pharmeasy-aloe-vera-juice-with-pulp-rejuvenates-skin-hair-1-litre-bottle-2-1680172150.jpg?dim=100x0&dpr=1&q=100",
+    img2 :"https://cdn01.pharmeasy.in/dam/products_otc/E54755/everherb-by-pharmeasy-aloe-vera-juice-with-pulp-rejuvenates-skin-hair-1-litre-bottle-6.1-1680172172.jpg?dim=100x0&dpr=1&q=100",
+    quantity : 1,
+    total : '199.00'
+   
+},
+{   id : 4,
+    name : "Everherb Amla Juice",
+    img_url : "https://cdn01.pharmeasy.in/dam/products_otc/D92359/everherb-by-pharmeasy-amla-juice-rich-in-vitamin-c-natural-immunity-booster-1-litre-bottle-2-1680238204.jpg?dim=700x0&dpr=1&q=100",
+    price : 399,
+    finalPrice : 199.50,
+    btn : "Add To Cart",
+    img1 : "https://cdn01.pharmeasy.in/dam/products_otc/D92359/everherb-by-pharmeasy-amla-juice-rich-in-vitamin-c-natural-immunity-booster-1-litre-bottle-2-1680238204.jpg?dim=100x0&dpr=1&q=100",
+    img2 :"https://cdn01.pharmeasy.in/dam/products_otc/D92359/everherb-by-pharmeasy-amla-juice-rich-in-vitamin-c-natural-immunity-booster-1-litre-bottle-6.1-1680172198.jpg?dim=100x0&dpr=1&q=100",
+    quantity : 1,
+    total : '199.50'
+    
+},
+{   id : 5,
+    name : "Liveasy Foods Healthy Seed Mix",
+    img_url : "https://cdn01.pharmeasy.in/dam/products_otc/E34104/liveasy-foods-healthy-seed-mix-blend-of-6-fibre-rich-healthy-seeds-2-1656420891.jpg?dim=700x0&dpr=1&q=100",
+    price : 399,
+    finalPrice : 219.50,
+    btn : "Add To Cart",
+    img1 :"https://cdn01.pharmeasy.in/dam/products_otc/E34104/liveasy-foods-healthy-seed-mix-blend-of-6-fibre-rich-healthy-seeds-2-1656420891.jpg?dim=100x0&dpr=1&q=100",
+    img2 :"https://cdn01.pharmeasy.in/dam/products_otc/E34104/liveasy-foods-healthy-seed-mix-blend-of-6-fibre-rich-healthy-seeds-3-1656420887.jpg?dim=100x0&dpr=1&q=100",
+    quantity : 1,
+    total : '219.50'
+    
+},
+{   id : 6,
+    name : "Chau Foods Chia Raw Seeds",
+    img_url : "https://cdn01.pharmeasy.in/dam/products_otc/R93251/jus-amazin-crunchy-seed-butter-mixed-seeds-with-flax-sunflower-seeds-125g85-mixed-seeds-2-1671745281.jpg?dim=700x0&dpr=1&q=100",
+    price : 299,
+    finalPrice : 269.50,
+    btn : "Add To Cart",
+    img1 :"https://cdn01.pharmeasy.in/dam/products_otc/R93251/jus-amazin-crunchy-seed-butter-mixed-seeds-with-flax-sunflower-seeds-125g85-mixed-seeds-2-1671745281.jpg?dim=100x0&dpr=1&q=100",
+    img2 :"https://cdn01.pharmeasy.in/dam/products_otc/R93251/jus-amazin-crunchy-seed-butter-mixed-seeds-with-flax-sunflower-seeds-125g85-mixed-seeds-6.1-1671745287.jpg?dim=100x0&dpr=1&q=100",
+    quantity : 1,
+    total : '269.50'
+    
+},
+{   id : 7,
+    name : "Pharmeasy All-In -One Vaporizer",
+    img_url :"https://cdn01.pharmeasy.in/dam/products_otc/Q83701/pharmeasy-all-in-one-vaporizer-1-1685623949.jpg?dim=700x0&dpr=1&q=100",
+    price : 599,
+    finalPrice : 399.50,
+    btn : "Add To Cart",
+    img1 :"https://cdn01.pharmeasy.in/dam/products_otc/Q83701/pharmeasy-all-in-one-vaporizer-1-1685623949.jpg?dim=100x0&dpr=1&q=100",
+    img2 :"https://cdn01.pharmeasy.in/dam/products_otc/Q83701/pharmeasy-all-in-one-vaporizer-2-1685623943.jpg?dim=100x0&dpr=1&q=100",
+    quantity : 1,
+    total : '399.50'
+    
+},
+{   id : 8,
+    name : "Liveasy Foods Healthy Seed Mix",
+    img_url : "https://cdn01.pharmeasy.in/dam/products_otc/E34104/liveasy-foods-healthy-seed-mix-blend-of-6-fibre-rich-healthy-seeds-2-1656420891.jpg?dim=700x0&dpr=1&q=100",
+    price : 349,
+    finalPrice : 319.50,
+    btn : "Add To Cart",
+    img1 :"https://cdn01.pharmeasy.in/dam/products_otc/E34104/liveasy-foods-healthy-seed-mix-blend-of-6-fibre-rich-healthy-seeds-2-1656420891.jpg?dim=100x0&dpr=1&q=100",
+    img2 :"https://cdn01.pharmeasy.in/dam/products_otc/E34104/liveasy-foods-healthy-seed-mix-blend-of-6-fibre-rich-healthy-seeds-3-1656420887.jpg?dim=100x0&dpr=1&q=100",
+    quantity : 1,
+    total : '319.50'
+    
+},
+   
+{   id : 9,
+    name : "Everherb (By Pharmeasy)",
+    img_url : "https://cdn01.pharmeasy.in/dam/products_otc/K40120/everherb-by-pharmeasy-pain-relief-oil-blend-of-8-powerful-herbal-ingredients-100-ml-2-1681539810.jpg",
+    price : 399.00,
+    finalPrice : 219.45,
+    btn : "Add To Cart",
+    img1 : "https://cdn01.pharmeasy.in/dam/products_otc/K40120/everherb-by-pharmeasy-pain-relief-oil-blend-of-8-powerful-herbal-ingredients-100-ml-2-1681539810.jpg?dim=100x0&dpr=1&q=100",
+    img2 : "https://cdn01.pharmeasy.in/dam/products_otc/K40120/everherb-by-pharmeasy-pain-relief-oil-blend-of-8-powerful-herbal-ingredients-100-ml-6.1-1681539844.jpg?dim=100x0&dpr=1&q=100",
+    quantity : 1,
+    total : '219.45'
+    
+},
+{   id : 10,
+    name : "Liveasy Pain Relief Balm",
+    img_url : "https://cdn01.pharmeasy.in/dam/products_otc/C93509/liveasy-essential-pain-relief-balm-45gm-2-1659963202.jpg",
+    price : 115.00,
+    finalPrice : 109.00,
+    btn : "Add To Cart",
+    img1 : "https://cdn01.pharmeasy.in/dam/products_otc/C93509/liveasy-essential-pain-relief-balm-45gm-2-1659963202.jpg?dim=100x0&dpr=1&q=100",
+    img2 : "https://cdn01.pharmeasy.in/dam/products_otc/C93509/liveasy-essential-pain-relief-balm-45gm-6.1-1659963200.jpg?dim=100x0&dpr=1&q=100",
+    quantity : 1,
+    total : '109.00'
+   
+},
+{   id : 11,
+    name : "Pharmeasy Multivitamin",
+    img_url : "https://cdn01.pharmeasy.in/dam/products_otc/E54733/pharmeasy-multivitamin-gummies-for-kids-and-adults-healthy-growth-immunity-bottle-of-30-2-1671743494.jpg",
+    price : 499.00,
+    finalPrice : 249.00,
+    btn : "Add To Cart",
+    img1 :"https://cdn01.pharmeasy.in/dam/products_otc/E54733/pharmeasy-multivitamin-gummies-for-kids-and-adults-healthy-growth-immunity-bottle-of-30-2-1671743494.jpg?dim=100x0&dpr=1&q=100",
+    img2 :"https://cdn01.pharmeasy.in/dam/products_otc/E54733/pharmeasy-multivitamin-gummies-for-kids-and-adults-healthy-growth-immunity-bottle-of-30-6.1-1671743491.jpg?dim=100x0&dpr=1&q=100",
+    quantity : 1,
+    total : '249.00'
+   
+},
+{   id : 12,
+    name : "Pharmeasy Biotin",
+    img_url : "https://cdn01.pharmeasy.in/dam/products_otc/D18575/pharmeasy-biotin-10000mcg-for-hair-skin-nails-bottle-of-60-2-1676757633.jpg",
+    price : 299.00,
+    finalPrice : 219.00,
+    btn : "Add To Cart",
+    img1 : "https://cdn01.pharmeasy.in/dam/products_otc/D18575/pharmeasy-biotin-10000mcg-for-hair-skin-nails-bottle-of-60-10-1676757634.jpg?dim=100x0&dpr=1&q=100",
+    img2 : "https://cdn01.pharmeasy.in/dam/products_otc/D18575/pharmeasy-biotin-10000mcg-for-hair-skin-nails-bottle-of-60-11-1676757628.jpg?dim=100x0&dpr=1&q=100",
+    quantity : 1,
+    total : '219.00'
+   
+},
+{   id : 13,
+    name : "Shelcal 500mg ",
+    img_url : "https://cdn01.pharmeasy.in/dam/products_otc/159115/shelcal-500mg-strip-of-15-tablets-2-1679999355.jpg",
+    price : 131.00,
+    finalPrice : 118.00,
+    btn : "Add To Cart",
+    img1 : "https://cdn01.pharmeasy.in/dam/products_otc/159115/shelcal-500mg-strip-of-15-tablets-2-1679999355.jpg?dim=100x0&dpr=1&q=100",
+    img2 : "https://cdn01.pharmeasy.in/dam/products_otc/159115/shelcal-500mg-strip-of-15-tablets-6.1-1679999314.jpg?dim=100x0&dpr=1&q=100",
+    quantity : 1,
+    total : '118.00'
+    
+},
+{   id : 14,
+    name : "Becozym C Forte",
+    img_url : "https://cdn01.pharmeasy.in/dam/products_otc/022290/becozym-c-forte-vitamin-b-complex-biotin-vitamin-c-strip-of-15-tablets-2-1671741220.jpg",
+    price : 27.4,
+    finalPrice : 24.00,
+    btn : "Add To Cart",
+    img1 : "https://cdn01.pharmeasy.in/dam/products_otc/022290/becozym-c-forte-vitamin-b-complex-biotin-vitamin-c-strip-of-15-tablets-2-1671741220.jpg?dim=100x0&dpr=1&q=100",
+    img2 : "https://cdn01.pharmeasy.in/dam/products_otc/022290/becozym-c-forte-vitamin-b-complex-biotin-vitamin-c-strip-of-15-tablets-6.1-1671741215.jpg?dim=100x0&dpr=1&q=100",
+    quantity : 1,
+    total : '24.00'
+    
+},
+{   id : 15,
+    name : "",
+    img_url : "https://cdn01.pharmeasy.in/dam/products_otc/051260/dabur-chyawanprash-250-gm-2-1669634957.jpg?dim=700x0&dpr=1&q=100",
+    price : 115.00,
+    finalPrice : 113.00,
+    btn : "Add To Cart",
+    img1 : "https://cdn01.pharmeasy.in/dam/products_otc/051260/dabur-chyawanprash-250-gm-2-1669634957.jpg?dim=100x0&dpr=1&q=100",
+    img2 : "https://cdn01.pharmeasy.in/dam/products_otc/051260/dabur-chyawanprash-250-gm-3-1669634953.jpg?dim=100x0&dpr=1&q=100",
+    quantity : 1,
+    total : '113.00'
+   
+},
+{   id : 16,
+    name : "Baidyanath Amla Juice",
+    img_url : "https://cdn01.pharmeasy.in/dam/products_otc/276008/baidyanath-amla-juice-immunity-omniprotection-to-virusno-added-sugarorganically-harvested-1l-1-1669634744.jpg?dim=700x0&dpr=1&q=100",
+    price : 250.00,
+    finalPrice : 240.00,
+    btn : "Add To Cart",
+    img1 : "https://cdn01.pharmeasy.in/dam/products_otc/276008/baidyanath-amla-juice-immunity-omniprotection-to-virusno-added-sugarorganically-harvested-1l-1-1669634744.jpg?dim=100x0&dpr=1&q=100",
+    img2 : "https://cdn01.pharmeasy.in/dam/products_otc/276008/baidyanath-amla-juice-immunity-omniprotection-to-virusno-added-sugarorganically-harvested-1l-2-1669634744.jpg?dim=100x0&dpr=1&q=100",
+    quantity : 1,
+    total : '240.00'
+    
+},
+]
+
+displayProduct(productArray3);
+
+function handleSortName3(){
+    var selected = document.getElementById("sortName-3").value;
+    if(selected === 'Ascending Order'){
+        productArray3.sort(function(a,b){
+            if(a.name>b.name){
+                return 1;
+            }
+            else if(a.name<b.name){
+                return -1;
+            }
+            else{
+                return 0;
+            }
+        })
+    }
+    if(selected === 'Descending Order'){
+        productArray3.sort(function(a,b){
+            if(a.name>b.name){
+                return -1;
+            }
+            else if(a.name<b.name){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        })
+    }
+    console.log(productArray3);
+    document.getElementById("parentAshu-3").innerHTML="";
+    displayProduct(productArray3);
+}
+
+function handleSortPrice3(){
+    var selected = document.getElementById("sortPrice-3").value;
+    // console.log(selected);
+    if(selected === 'Low to High'){
+        productArray3.sort(function(a,b){
+         return a.finalPrice - b.finalPrice;
+        });
+    }
+    if(selected === 'High to Low'){
+        productArray3.sort(function(a,b){
+         return b.finalPrice - a.finalPrice;
+        });
+    }
+    // console.log(productArray);
+    document.getElementById("parentAshu-3").innerHTML="";
+    displayProduct(productArray3);
+}
+
+function displayProduct(){
+    productArray3.map(function(elem){
+        var div = document.createElement("div");
+        var image = document.createElement("img");
+        image.setAttribute("src",elem.img_url);
+        image.setAttribute("alt",elem.name);
+
+        var name = document.createElement("h3");
+        name.textContent = elem.name;
+
+        var priceBox = document.createElement("div");
+
+        var finalPrice = document.createElement("p");
+        finalPrice.textContent = "MRP ₹ "+elem.finalPrice;
+
+        var price = document.createElement("p");
+        price.textContent = "MRP ₹ "+ elem.price;
+
+        var button = document.createElement("button");
+        button.textContent = elem.btn;
+
+        button.addEventListener("click",function(){
+            addtocart3(elem);
+        });
+
+
+        div.addEventListener("click",function(){
+            displayInfo(elem);
+        })
+        div.addEventListener("click",function(){
+              navToNextPage(elem);
+            });
+
+        priceBox.append(price,finalPrice);
+
+        div.append(image,name,priceBox,button);
+        document.getElementById("parentAshu-3").append(div);
+
+    })
+}
+
+function addtocart3(elem){
+var cartarr3 = JSON.parse(localStorage.getItem("cartproducts")) || [];
+cartarr3.push(elem);
+localStorage.setItem("cartproducts", JSON.stringify(cartarr3));
+}
+
+var product_desc = [];
+      function navToNextPage(product){
+      product_desc.push(product);
+      localStorage.setItem("Prodct-Data",JSON.stringify(product_desc));
+      window.location.href="../PharmEasy/monu/Product-Details/product-detail.html";
+  }
+
+var products = JSON.parse(localStorage.getItem("productInfo")) || [];
+
+function displayInfo(productArray3){
+    products.push(productArray3)
+    localStorage.setItem("productInfo",JSON.stringify(products));
+    link.setAttribute("href",productDetails.html);   
+}
